7cf9e305c3b3906f91251f704131986ec9f0c3c8
Pinal <u2232690@uel.ac.uk>
2023-01-19 12:50:44 +0000

    Initial Commit

---
+++ Tesla app/Tesla_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tesla_appApp.swift
+//  Tesla app
+//
+//  Created by Pinal on 19/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct Tesla_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
